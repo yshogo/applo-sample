2f245b03a539201bfa263c9bc6e3ba2f0033f050
yshogo <superman199323@gmail.com>
2019-01-18 15:57:58 +0900

    Apolloでデータを送信する処理の作成

--- app/src/main/java/com/example/shogoyamada/applo_sample_app/MainActivity.kt
+++ app/src/main/java/com/example/shogoyamada/applo_sample_app/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.shogoyamada.applo_sample_app
 
+import AddReactionToIssueMutation
 import LoginQuery
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
+import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
+import type.ReactionContent
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val okHttpClient = OkHttpClient.Builder()
-            .authenticator{ _, response ->
+            .authenticator { _, response ->
                 response.request().newBuilder().addHeader("Authorization", "Bearer $GITHUB_ACCESS_TOKENS")
                     .build()
             }.build()
@@ -34,15 +37,33 @@ class MainActivity : AppCompatActivity() {
 
         val query = LoginQuery.builder().build()
 
-        apolloClient.query(query).enqueue(object: ApolloCall.Callback<LoginQuery.Data>() {
+        apolloClient.query(query).enqueue(object : ApolloCall.Callback<LoginQuery.Data>() {
             override fun onFailure(e: ApolloException) {
                 Log.e("エラーが発生しました。", e.toString())
             }
 
             override fun onResponse(response: Response<LoginQuery.Data>) {
                 val viewer = response.data()?.viewer()
-                print(viewer)
+                val userLogin = viewer?.login()
+                print(userLogin)
             }
         })
+
+        button.setOnClickListener {
+            val mutation = AddReactionToIssueMutation.builder()
+                .subjectId("MDU6SXNzdWU0MDA1ODMwODk=")
+                .content(ReactionContent.THUMBS_DOWN)
+                .build()
+
+            apolloClient.mutate(mutation).enqueue(object : ApolloCall.Callback<AddReactionToIssueMutation.Data>() {
+                override fun onFailure(e: ApolloException) {
+                    print(e.toString())
+                }
+
+                override fun onResponse(response: Response<AddReactionToIssueMutation.Data>) {
+                    print("成功")
+                }
+            })
+        }
     }
 }
